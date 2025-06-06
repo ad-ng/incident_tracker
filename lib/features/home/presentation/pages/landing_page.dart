31c413390f260bbbe83cdaf975785441f6439ba5
ad-ng <adolphengoga@gmail.com>
2025-06-06 10:31:36 +0200

    finishing up add an incident ui

--- lib/features/home/presentation/pages/landing_page.dart
+++ lib/features/home/presentation/pages/landing_page.dart
@@ -11,11 +11,151 @@ class LandingPage extends StatefulWidget {
 class _LandingPageState extends State<LandingPage> {
   int currentIndex = 0;
 
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
+  TextEditingController locationController = TextEditingController();
+  TextEditingController dateController = TextEditingController();
+
   List pages = [
     HomePage(),
     Center(child: Text(' P R O F I L E')),
     Center(child: Text(' S E T T I N G S')),
   ];
+
+  void openModel() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        String _categoryDropDownValue = 'High priority';
+        List<String> categoryItems = [
+          'High priority',
+          'Priority',
+          'Low Priority',
+        ];
+
+        String _statusDropDownValue = 'Open';
+        List<String> statusItems = ['Open', 'Closed'];
+
+        return AlertDialog(
+          title: Text('Add a New Incident'),
+          content: Container(
+            height: 380,
+            child: Form(
+              child: StatefulBuilder(
+                builder: (BuildContext context, StateSetter setModalState) {
+                  return SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        TextFormField(
+                          controller: titleController,
+                          decoration: InputDecoration(
+                            focusedBorder: OutlineInputBorder(),
+                            enabledBorder: OutlineInputBorder(),
+                            labelText: 'Title',
+                          ),
+                        ),
+                        SizedBox(height: 8),
+                        TextFormField(
+                          controller: descriptionController,
+                          decoration: InputDecoration(
+                            focusedBorder: OutlineInputBorder(),
+                            enabledBorder: OutlineInputBorder(),
+                            labelText: 'Description',
+                          ),
+                        ),
+                        SizedBox(height: 8),
+                        TextFormField(
+                          controller: locationController,
+                          decoration: InputDecoration(
+                            focusedBorder: OutlineInputBorder(),
+                            enabledBorder: OutlineInputBorder(),
+                            labelText: 'Location',
+                          ),
+                        ),
+                        SizedBox(height: 8),
+                        TextFormField(
+                          controller: dateController,
+                          decoration: InputDecoration(
+                            focusedBorder: OutlineInputBorder(),
+                            enabledBorder: OutlineInputBorder(),
+                            labelText: 'Date & Time',
+                          ),
+                        ),
+                        SizedBox(height: 8),
+                        Row(
+                          children: [
+                            Text("Category:", style: TextStyle(fontSize: 16)),
+                            SizedBox(width: 12),
+                            Expanded(
+                              child: DropdownButton<String>(
+                                isExpanded: true,
+                                value: _categoryDropDownValue,
+                                items:
+                                    categoryItems.map((String item) {
+                                      return DropdownMenuItem(
+                                        value: item,
+                                        child: Text(item),
+                                      );
+                                    }).toList(),
+                                onChanged: (String? newValue) {
+                                  setModalState(() {
+                                    _categoryDropDownValue = newValue!;
+                                  });
+                                },
+                              ),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Text("Status:   ", style: TextStyle(fontSize: 16)),
+                            SizedBox(width: 12),
+                            Expanded(
+                              child: DropdownButton<String>(
+                                isExpanded: true,
+                                value: _statusDropDownValue,
+                                items:
+                                    statusItems.map((String item) {
+                                      return DropdownMenuItem(
+                                        value: item,
+                                        child: Text(item),
+                                      );
+                                    }).toList(),
+                                onChanged: (String? newValue) {
+                                  setModalState(() {
+                                    _statusDropDownValue = newValue!;
+                                  });
+                                },
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('Save'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,6 +169,13 @@ class _LandingPageState extends State<LandingPage> {
         ],
       ),
       body: pages[currentIndex],
+      floatingActionButton:
+          (currentIndex == 0)
+              ? FloatingActionButton(
+                onPressed: openModel,
+                child: Icon(Icons.add),
+              )
+              : SizedBox.shrink(),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: currentIndex,
         onTap: (value) {
