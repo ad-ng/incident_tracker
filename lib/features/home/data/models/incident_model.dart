ec0148c2b541ebcb2366129ade69eebd8f1e884a
ad-ng <adolphengoga@gmail.com>
2025-06-06 08:31:49 +0200

    adding incident model

---
+++ lib/features/home/data/models/incident_model.dart
@@ -0,0 +1,58 @@
+import 'dart:convert';
+
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class IncidentModel {
+  int id;
+  String title;
+  String description;
+  String category;
+  String priority;
+  String Location;
+  DateTime dateTime;
+  String status;
+  String photo;
+  IncidentModel({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.category,
+    required this.priority,
+    required this.Location,
+    required this.dateTime,
+    required this.status,
+    required this.photo,
+  });
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'title': title,
+      'description': description,
+      'category': category,
+      'priority': priority,
+      'Location': Location,
+      'dateTime': dateTime.millisecondsSinceEpoch,
+      'status': status,
+      'photo': photo,
+    };
+  }
+
+  factory IncidentModel.fromMap(Map<String, dynamic> map) {
+    return IncidentModel(
+      id: map['id'] as int,
+      title: map['title'] as String,
+      description: map['description'] as String,
+      category: map['category'] as String,
+      priority: map['priority'] as String,
+      Location: map['Location'] as String,
+      dateTime: DateTime.fromMillisecondsSinceEpoch(map['dateTime'] as int),
+      status: map['status'] as String,
+      photo: map['photo'] as String,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory IncidentModel.fromJson(String source) =>
+      IncidentModel.fromMap(json.decode(source) as Map<String, dynamic>);
+}
