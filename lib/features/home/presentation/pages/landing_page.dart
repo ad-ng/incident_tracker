f95fddda3fec6e6119c736bb2384529dcec61274
ad-ng <adolphengoga@gmail.com>
2025-06-06 08:16:03 +0200

    adding battom nav bar

---
+++ lib/features/home/presentation/pages/landing_page.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class LandingPage extends StatefulWidget {
+  const LandingPage({super.key});
+
+  @override
+  State<LandingPage> createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage> {
+  int currentIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Incident tracker'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.logout_outlined, size: 35),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+        onTap: (value) {
+          setState(() {
+            currentIndex = value;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home_filled), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+          BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Setting'),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:incident_tracker/features/auth/data/repository/auth_repo_impl.dart';
 import 'package:incident_tracker/features/auth/presentation/bloc/auth_cubit.dart';
-import 'package:incident_tracker/features/auth/presentation/pages/loginPage.dart';
+import 'package:incident_tracker/features/home/presentation/pages/landing_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         ),
-        home: LogInPage(),
+        home: LandingPage(),
       ),
     );
   }
